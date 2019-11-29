82d4aa591d9f003647def13e0ca34b9d450eedce
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2019-11-29 17:30:41 +0200

    +modif

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufReader, BufWriter, Write, Read};
 use std::os::unix::net::{UnixStream,UnixListener};
 use std::thread;
-use std::ops::Add;
 
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use byteorder::*;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 enum MsgTypes {
-    register,
-    ok,
-    command,
+    Register,
+    Ok,
+    Command,
+    Close,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -20,90 +21,96 @@ struct Message {
     value: String,
 }
 
-fn register_client(msg: &Message, clients: &Arc<Mutex<HashMap<String, MsgTypes>>>) {
+fn read_message(stream: &UnixStream) -> String {
+    let mut read = BufReader::new(stream);
+    let mut msg_size = read.read_u64::<BigEndian>().unwrap() as usize;
+    let mut line = String::from("");
+    line.reserve(msg_size);
+
+    while msg_size > 0 {
+        let mut buffer = [0u8; 1024];
+        let size = read.read(&mut buffer).unwrap();
+        for ch in 0..size {
+            line.push(char::from(buffer[ch]));
+        }
+        msg_size = msg_size - size;
+    }
+    println!("reading");
+    return line;
+}
+
+fn write_message(stream: &UnixStream, message: &String) -> usize {
+    let mut write = BufWriter::new(stream);
+    let msg_size = message.as_bytes().len();
+    if write.write_u64::<BigEndian>(msg_size as u64).is_err() {
+        println!("Can't write to broker");
+        return 0;
+    }
+    if write.write_all(message.as_bytes()).is_err() {
+        println!("Can't write to broker");
+        return 0;
+    }
+    if write.flush().is_err() {
+        println!("Can't flush");
+        return 0;
+    }
+    println!("writing");
+    return msg_size;
+}
+
+fn register_client(stream: &UnixStream, msg: &Message, clients: &Arc<Mutex<HashMap<String, MsgTypes>>>) {
     let tmp = msg.clone();
     clients.lock().unwrap().insert(tmp.value, tmp.header);
 
-    let client = UnixStream::connect(&msg.value).unwrap();
-    let mut write_client = BufWriter::new(&client);
-
-    write_client.write(serde_json::to_string(&msg).unwrap().as_bytes());
-    write_client.write(b"\n");
-    write_client.flush();
+    let close = Message { header: MsgTypes::Close, value: String::from(&msg.value) };
+    {
+        let msg = serde_json::to_string(&close).unwrap();
+        write_message(stream, &msg);
+        println!("{}", msg);
+    }
 }
 
 fn broadcast_command(msg: &Message, clients: &Arc<Mutex<HashMap<String, MsgTypes>>>, stream: &UnixStream) {
-    let mut write_server = BufWriter::new(stream);
-
     let w = clients.lock().unwrap();
     for c in w.iter() {
         let client = UnixStream::connect(c.0).unwrap();
-        let mut write_client = BufWriter::new(&client);
+        let msg = serde_json::to_string(&msg).unwrap();
+        write_message(&client, &msg);
 
-        write_client.write(serde_json::to_string(&msg).unwrap().as_bytes());
-        write_client.write(b"\n");
-        write_client.flush();
+        let line = read_message(&client);
 
-        let read_client = BufReader::new(&client);
-        let trs = read_client;
-        for client_res in trs.lines() {
-            match client_res {
-                Ok(mut line) => {
-                    if line.len() == 0 {
-                        break;
-                    }
-                    println!("recv {}", line);
-
-                    line = line.add("\n");
-                    let bytes_wrote: usize = write_server.write(line.as_bytes()).unwrap();
-                    println!("{}", bytes_wrote);
-                    write_server.flush();
-                },
-                _ => {}
-            }
-        }
+        write_message(&stream, &line);
     }
 }
 
 fn handle_client(stream: UnixStream, clients: Arc<Mutex<HashMap<String, MsgTypes>>>) {
-    let read_server = BufReader::new(&stream);
-    let mut write_server = BufWriter::new(&stream);
+    let line = read_message(&stream);
 
-    let srs = read_server;
-    for res in srs.lines() {
-        match res {
-            Ok(mut line) => {
-                if line.len() == 0 {
-                    break;
-                }
+    let msg = serde_json::from_str(&line.as_str());
+    let msg: Message = match msg {
+        Ok(msg) => {
+            msg
+        },
+        _ => {
+            println!("{} {}", line, "Bad Message!");
+            return;
+        }
+    };
 
-                let msg = serde_json::from_str(&line.as_str());
-                let msg: Message = match msg {
-                    Ok(msg) => {
-                        msg
-                    },
-                    _ => {
-                        println!("{}", "Bad Message!");
-                        return;
-                    }
-                };
-
-                match msg.header {
-                    MsgTypes::register => {
-                        register_client(&msg, &clients);
-                    },
-                    _ => {
-                        broadcast_command(&msg, &clients, &stream);
-                    }
-                }
-            },
-            _ => {}
+    match msg.header {
+        MsgTypes::Register => {
+            register_client(&stream, &msg, &clients);
+        },
+        _ => {
+            broadcast_command(&msg, &clients, &stream);
         }
     }
 }
 
 fn remove_socket(path: &String) {
-    std::fs::remove_file(path);
+    if std::fs::remove_file(path).is_err() {
+        panic!("Can't remove socket {}", path);
+    }
 }
 
 fn main() {
